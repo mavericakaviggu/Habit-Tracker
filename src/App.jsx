79471769c35453c46840b13ede599a6392f616ad
Vignesh <vigneshshet4577@gmail.com>
2026-03-07 14:59:06 +0530

    sign up page skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Todo from './Components/Todo'
+import LoginSignUp from './Components/LoginSignUp'
 
 const App = () => {
   return (
     <div>
-      <Todo />
+      {/* <Todo /> */}
+      <LoginSignUp />
     </div>
   );
 };
--- src/Components/LoginSignUp.jsx
+++ src/Components/LoginSignUp.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import './CSS/LoginSignUp.css'
+import email from './Assets/email.png';
+import lock from './Assets/lock.png';
+import person from './Assets/person.png';
 
 const LoginSignUp = () => {
   return (
-    <div>
-      
+    <div className = 'container'>
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={person} alt="Person" />
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="input">
+          <img src={email} alt="Email" />
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="input">
+          <img src={lock} alt="Lock" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="forgot-password">Forgot Password? <span>Click here</span></div>
+        <div className="submit-container">
+          <button type="submit">Sign Up</button>
+          <button type="button">Login</button>
+        </div>
+      </div>
     </div>
   )
 }
