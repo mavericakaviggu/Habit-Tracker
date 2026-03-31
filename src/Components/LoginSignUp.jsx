bf06c50e247786c08b77ea0a077f15c990c60f53
Vignesh <vigneshshet4577@gmail.com>
2026-03-31 13:18:33 +0530

    dynamic display of button in loin/sign up page

--- src/Components/LoginSignUp.jsx
+++ src/Components/LoginSignUp.jsx
@@ -5,10 +5,13 @@ import lock from './Assets/lock.png';
 import person from './Assets/person.png';
 
 const LoginSignUp = () => {
+
+  const [action,setAction] = React.useState('Sign Up');
+
   return (
     <div className = 'container'>
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
@@ -26,8 +29,8 @@ const LoginSignUp = () => {
         </div>
         <div className="forgot-password">Forgot Password? <span>Click here</span></div>
         <div className="submit-container">
-          <div className="submit">Sign Up</div>
-          <div className="submit">Login</div>
+          <div className={action==="Login" ? "submit gray" : "submit"}>Sign Up</div>
+          <div className={action==="Sign Up" ? "submit gray" : "submit"}>Login</div>
         </div>
       </div>
     </div>
